8289e12c8274e060319cc982bc0d5600ab021df2
me2wdo <159659732+me2wdo@users.noreply.github.com>
2024-05-25 14:49:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://dashboard.heroku.com/new?template=https://github.com/me2wdo/meska.git",
+    "https://github.com/me2wdo/meska.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
